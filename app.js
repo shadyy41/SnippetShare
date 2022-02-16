6ad41d9455ea84d0282a658daa7ff5f4861dab3b
shady41 <abhinavjha2512@gmail.com>
2022-02-16 13:22:20 +0530

    Removed secure cookie.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true,
+    // secure: true,
     expires: Date.now()+ 1000 * 60 * 60 * 24 * 7, //expires in a week (in miliseconds)
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
